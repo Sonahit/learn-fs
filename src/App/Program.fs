cb123fda4bb880e7e7be23e1a873112b75d6ae5b
Ivan Sadykov <grandpajok@gmail.com>
2021-01-09 08:07:23 +0600

    feat: create app

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,47 +1,48 @@
 open System
 open System.Threading
 open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
 open XPlot.Plotly
 
-let layout = Layout(title = "Basic Bar Chart")
+let layout = Layout(title = "Product - Price")
 
 [<EntryPoint>]
 let main argv =
-
-    let csvData = Csv.genData 100
-
-    let data =
-        csvData
-        |> Seq.sortBy (fun (_, price, __, ___, ____) -> price)
-        |> Seq.toList
-
-    let trace =
-        Scatter(
-            x = List.toArray (List.map (fun (product, _, __, ___, ____) -> product) data),
-            y = List.toArray (List.map (fun (_, price, __, ___, ____) -> price) data)
-        )
-
-    let plot =
-        trace
-        |> Chart.Plot
-        |> Chart.WithLayout layout
-        |> Chart.WithHeight 500
-        |> Chart.WithWidth 700
-        |> Chart.WithWidth 700
-
-    let html = plot.GetHtml()
-
     let cts = new CancellationTokenSource()
 
     let conf =
         { defaultConfig with
               cancellationToken = cts.Token }
 
-    let listening, server =
-        startWebServerAsync conf (Successful.OK html)
+    let getPlot (rows: int) =
+        let csvData = Csv.genData rows
+
+        let data =
+            csvData
+            |> Seq.sortByDescending (fun (_, price, __, ___, ____) -> price)
+            |> Seq.toList
+
+        let trace =
+            Scatter(
+                x = List.toArray (List.map (fun (product, _, __, ___, ____) -> product) data),
+                y = List.toArray (List.map (fun (_, price, __, ___, ____) -> price) data)
+            )
+
+        trace
+        |> Chart.Plot
+        |> Chart.WithLayout layout
+        |> Chart.WithHeight 500
+        |> Chart.WithWidth 1000
+
+    let app =
+        choose [ GET
+                 >=> choose [ path "/" >=> OK(getPlot(10).GetHtml()) ] ]
+
+    let listening, server = startWebServerAsync conf app
 
     Async.Start(server, cts.Token)
-    printfn "Make requests now"
     Console.ReadKey true |> ignore
 
     cts.Cancel()
